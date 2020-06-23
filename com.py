d74f4c0c0a8c1e6eb1b8a07bc52ca4dc9b4b75ae
ryan <ryantbate@gmail.com>
2020-06-22 21:38:18 -0500

    Added ledPacket class for transmission

--- com.py
+++ com.py
@@ -1,5 +1,27 @@
 import socket as sk
 import led
 
-def sendToStrip(clr):
-    rgb = clr.get()
+
+
+class ledPacket:
+    command = "off"
+    data = []
+    def __init__(self, cmd="off", d=[]):
+        command = cmd
+        data = d
+
+    def _cmd2byte(self, command):
+        cmd = {
+            "off"       :   0x00,
+            "solid"     :   0x01,
+            "gradient"  :   0x02
+        }
+        return cmd.get(command, 0x00)
+
+    def pack(self):
+        
+
+    
+    
+
+
