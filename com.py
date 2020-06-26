ce03315b7b05af71640078f72bab4a04712cdb9f
Ryan B <ryan.bate@hotmail.com>
2020-06-26 00:26:56 -0500

    Added packet sending protocol

--- com.py
+++ com.py
@@ -1,25 +1,49 @@
 import socket as sk
 import led
 
+HEADER = 'abab'
+FOOTER = 'cdcd'
 
+def send(colour1, colour2, command, ip, port):
+    print("Getting ready to send")
+    outPkt = ledPacket(command, [colour1, colour2], ip, port)
+    outPkt.send()
+    pass
 
 class ledPacket:
+    ip = None
+    port = 0
     command = "off"
     data = []
-    def __init__(self, cmd="off", d=[]):
-        command = cmd
-        data = d
 
-    def _cmd2byte(self, command):
+    def __init__(self, cmd="off", d=[], ip=None, port=0):
+        self.ip = ip
+        self.port = int(port)
+        self.command = cmd
+        self.data = d
+        print("packing data: " + cmd + str(d) + "\nsending to" + ip + port)
+        
+    def _cmd2byte(self):
         cmd = {
-            "off"       :   0x00,
-            "solid"     :   0x01,
-            "gradient"  :   0x02
+            "off"       :   '00',
+            "solid"     :   '01',
+            "gradient"  :   '02'
         }
-        return cmd.get(command, 0x00)
+        return cmd.get(self.command, 0x00)
 
     def pack(self):
-        
+        print("packing payload...")
+        payload = HEADER + self._cmd2byte() + self.data[0].getHexAsStr() + self.data[1].getHexAsStr() + FOOTER
+        print("payload = " + payload)
+        return payload
+
+    def send(self):
+        print("opening socket and sending")
+        sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+        sock.sendto(bytes.fromhex(self.pack()), (self.ip, self.port))
+        print("done sending!")
+        sock.close()
+        return 1
 
     
     
