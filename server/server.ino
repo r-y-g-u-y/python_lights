4c86ca048d7d0b41f48af145ee66169ea0ed5884
ryan <ryantbate@gmail.com>
2020-06-27 11:18:25 -0500

    Added rudimentary LED support

--- server/server.ino
+++ server/server.ino
@@ -1,3 +1,8 @@
+#include <NeoPixelAnimator.h>
+#include <NeoPixelBrightnessBus.h>
+#include <NeoPixelBus.h>
+#include <NeoPixelSegmentBus.h>
+
 #include "led.h"
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
@@ -5,7 +10,11 @@
 const char* ssid = "BateLAN";
 const char* password = "7536131077";
 
+const uint16_t PixelCount = 32;
+const uint8_t PixelPin = 2;
 
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+RgbColor off(0);
 
 
 WiFiUDP Udp;
@@ -22,16 +31,33 @@ void parse_command(s_light *command)
   switch(command->command)
   {
     case CMD_OFF:
+      handle_led_off();
       break;
     case CMD_SOLID:
+      handle_led_solid(command);
       break;
     case CMD_GRADIENT:
+      handle_led_gradient(command);
       break; 
   }
 }
 
 void handle_led_solid(s_light *command)
 {
+  RgbColor color1(command->red_1, command->green_1, command->blue_1);
+  for (int i=0; i<PixelCount; i++)
+  {
+    strip.SetPixelColor(i, color1);
+  }
+}
+
+void handle_led_off()
+{
+  for (int i=0; i<PixelCount; i++)
+  {
+    strip.SetPixelColor(i, off);
+  }
+  strip.show()
 }
 
 
@@ -52,6 +78,12 @@ void setup()
 
   Udp.begin(localUdpPort);
   Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+  delay(200);
+  strip.Begin();
+  strip.Show();
+  delay(200);
+  Serial.println("LED Strip now running");
+  
 }
 
 void loop()
